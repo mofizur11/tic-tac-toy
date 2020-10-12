bfdfcf0b593b2c2253b6d45852c02449d4b97e6f
Mofizur Rahman <mofizur853@gmail.com>
2020-10-12 22:12:19 +0600

    Initial commit

---
+++ src/ActivityMain.java
@@ -0,0 +1,11 @@
+
+public class ActivityMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		GuessGame game = new GuessGame();
+		game.startGame();
+	}
+
+}
---
+++ src/Player.java
@@ -0,0 +1,11 @@
+
+public class Player {
+	
+	int number = 0; //where the quess goes
+	
+	public void guess() {
+		number = (int) (Math.random() * 10);
+		System.out.println("I'm guessign " + number);
+	}
+
+}
